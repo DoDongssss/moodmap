ef0521756dbf7736e57390d5af9e4071c2e2e2db
johnny.asumbra <johnny@sdsolutions.com.ph>
2025-12-02 15:38:20 +0800

    remove useMemo in backtotop

--- src/components/ui/BackToTop.tsx
+++ src/components/ui/BackToTop.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { rafThrottle } from '../../utils/performanceOptimizations';
 import styled from "styled-components";
